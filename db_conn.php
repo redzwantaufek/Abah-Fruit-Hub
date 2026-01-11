c41163b3cd5d05e45a5896c91a2f3cc8e85b286b
naufalqwerty123 <muhdnaufalx@gmail.com>
2026-01-11 23:18:20 +0800

    change name

---
+++ db_conn.php
@@ -0,0 +1,18 @@
+<?php
+
+/* php & Oracle DB connection file */
+
+$user = "SYSTEM";
+$password = "123";
+$database = "localhost/FREEPDB1"; // Format: host/service_name
+
+$dbconn = oci_connect($username, $password, $database);
+
+if (!$dbconn) {
+    $e = oci_error();
+    trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+} else {
+    echo "ORACLE DATABASE CONNECTED SUCCESSFULLY!!!";
+}
+   
+?>
